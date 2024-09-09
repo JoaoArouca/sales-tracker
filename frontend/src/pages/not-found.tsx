f829fc0058e35d296c4de3e553b876730dbf55b9
João Victor <jarouca21@gmail.com>
2024-09-08 21:25:46 -0300

    front-feature: not found page

---
+++ frontend/src/pages/not-found.tsx
@@ -0,0 +1,18 @@
+import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
+
+export function NotFound() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-2">
+      <Helmet title='404 - Not Found' />
+      <h1 className="text-4xl font-bold">Page was not found</h1>
+      <p className="text-accent-foreground">
+        Get back to{' '}
+        <Link className="text-sky-500 dark:text-sky-400" to="/">
+          dashboard
+        </Link>
+        .
+      </p>
+    </div>
+  )
+}
