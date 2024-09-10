0661a04204cfde402378ce41f0154c706466f1ea
João Victor <jarouca21@gmail.com>
2024-09-10 20:43:28 -0300

    front-feature: Dashboard page infinite scroll structure

--- frontend/src/lib/routes.tsx
+++ frontend/src/lib/routes.tsx
@@ -1,11 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import { NotFound } from "../pages/not-found";
 import { AppLayout } from "@/pages/_layouts/app";
+import { Dashboard } from "@/pages/dashboard";
 
 export const router = createBrowserRouter([
   {
     path: '/',
     errorElement: <NotFound />,
     element: <AppLayout />,
+    children: [
+      {
+        path: '',
+        element: <Dashboard />
+      }
+    ]
   }
 ])
