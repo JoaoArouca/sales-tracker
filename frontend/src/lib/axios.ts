c638fb2c454b987eb93bc8c90fb5445eb55a9981
João Victor <jarouca21@gmail.com>
2024-09-10 20:42:38 -0300

    front-feature: add delay to api

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,16 @@
 import axios from "axios"
 import { env } from "./env"
 
+const LAZY_TIME = 3000
+
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true
+})
+
+api.interceptors.request.use(async (config) => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, LAZY_TIME)
+  })
+  return config
 })
